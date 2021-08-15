97ec1a205dab01c5347f73be756320832c295a8e
EUNHEE JANG <6810779s@naver.com>
2021-08-15 14:33:35 +0900

    add

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 import TodoTemplate from './components/TodoTemplate';
 
 const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: '할일1',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: '할일2',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: '할일3',
+      checked: false,
+    },
+  ]);
   return (
     <TodoTemplate>
       <TodoInsert />
-      <TodoList />
+      <TodoList todos={todos} />
     </TodoTemplate>
   );
 };
--- src/components/TodoInsert.js
+++ src/components/TodoInsert.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { MdAdd } from 'react-icons/md';
 import '../style/TodoInsert.css';
 
 const TodoInsert = () => {
+  const [value, setValue] = useState('');
+  const onChange = useCallback((e) => {
+    setValue(e.target.value);
+  }, []);
   return (
     <div className="TodoInsert">
-      <input placeholder="할 일을 입력하세요" />
+      <input
+        placeholder="할 일을 입력하세요"
+        value={value}
+        onChange={onChange}
+      />
       <button type="submit">
         <MdAdd />
       </button>
